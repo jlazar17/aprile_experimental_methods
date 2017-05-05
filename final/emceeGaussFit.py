48466f8a69a2b5f3fa5777b5583f260378fe2d89
Alexis Nelson <alexisnelson@Alexiss-MacBook-Pro.local>
2017-05-05 18:51:06 -0400

    whatever

--- final/emceeGaussFit.py
+++ final/emceeGaussFit.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 # This module was written based on a tutorial by Dan Foreman-Mackey and 
-# contributors found here http://dan.iel.fm/emcee/current/user/line/ 
+# contributors found at http://dan.iel.fm/emcee/current/user/line/ 
 ################################################################################
 
 import numpy as np
@@ -14,41 +14,44 @@ NDIM = 4
 ################################################################################
 ###################### FIT GAUSSIAN USING SCIPY.OPTIMIZE #######################
 
-def gaussian(x, a, mu, sig):
-    return a*np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+def gauss(x, p):
+	A, mu, sigma = p
+	return A*np.exp(-(x-mu)**2/(2.*sigma**2))
 
 def lnlike(theta, x, y, yerr):
 	A, mu, sigma, lnf = theta
-	model = gaussian(x,A,mu,sigma)
+	model = gauss(x,(A,mu,sigma))
 	inv_sigma2 = 1.0/(yerr**2 + model**2*np.exp(2*lnf))
 	return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
 
-def findParameters(A0,m0,sigma0,f0):
+def findParameters(x,y,yerr,A0,mu0,sigma0,lnf0):
 	nll = lambda *args: -lnlike(*args)
-	result = op.minimize(nll, [A0, mu0, sigma0,np.log(f0)], args=(x, y, yerr))
+	result = op.minimize(nll, [A0, mu0, sigma0, lnf0], args=(x, y, yerr))
 #	A_ml, mu_ml, sigma_ml, lnf_ml = result["x"]
 	return result["x"]
 
-# TODO CHANGE THESE VALUES
-def lnprior(theta):
-	m, b, lnf = theta
-	if -5.0 < m < 0.5 and 0.0 < b < 10.0 and -10.0 < lnf < 1.0:
+def lnprior(theta, thetaLower, thetaUpper):
+	A, mu, sig, lnf = theta
+	AL, muL, sigL, lnfL = thetaLower
+	AU, muU, sigU, lnfU = thetaUpper
+	if AL < A < AU and muL < mu < muU and lnfL < lnf < lnfU and sigL < sig<sigU:
 		return 0.0
 	return -np.inf
 
-def lnprob(theta, x, y, yerr):
-	lp = lnprior(theta)
+def lnprob(theta, thetaL, thetaU, x, y, yerr):
+	lp = lnprior(theta, thetaL, thetaU)
 	if not np.isfinite(lp):
 		return -np.inf
 	return lp + lnlike(theta, x, y, yerr)
 
-def uncertainties(nwalkers,theta0, x, y, yerr):
-	result = findParamete(*theta0)
+def uncertainties(nwalkers,theta0, thetaL, thetaU, x, y, yerr):
+	result = findParameters(x,y,yerr,*theta0)
+	print(result)
 	pos = [result + 1e-4*np.random.randn(NDIM) for i in range(nwalkers)]
-	sampler = emcee.EnsembleSampler(nwalkers, NDIM, lnprob, args=(x, y, yerr))
+	sampler = emcee.EnsembleSampler(nwalkers, NDIM, lnprob,
+                                    args=(thetaL, thetaU, x, y, yerr))
 	sampler.run_mcmc(pos, 500)
-	samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+	samples = sampler.chain[:, 50:, :].reshape((-1, NDIM))
 	samples[:, 2] = np.exp(samples[:, 2])
-	Amcmc, mumcmc, sigmcmc, fmcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-										zip(*np.percentile(samples, [16, 50, 84],
-														   axis=0)))
+	return map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), 
+			   zip(*np.percentile(samples, [16, 50, 84],axis=0)))
--- final/fit_spectrum.py
+++ final/fit_spectrum.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+import emceeGaussFit as gfit
 
 ################################################################################
 ############################## PREPARING ARRAY #################################
@@ -17,12 +18,19 @@ aCut = a[cutInd]
 n, bins = np.histogram(aCut,bins=50)
 binMids = [ (bins[i]+bins[i+1])/2 for i in range(len(bins)-1) ]
 binWidth = (bins[1]-bins[0])/2
-yErrs = [ x**0.5 for x in n ]
+yErrs = np.array([ x**0.5 for x in n ])
 
 
 ################################################################################
 ############################# FITTING GAUSSIAN #################################
 
+# numbers based on those in assignment
+theta = gfit.findParameters(binMids,n,yErrs,160,32910,879,0.5)
+thetaL = (125,31000,700,-10)
+thetaU = (200,35000,1000,1)
+Amcmc, mumcmc, sigmcmc, lnfmcmc = gfit.uncertainties(100,theta,thetaL,thetaU,
+													 binMids,n,yErrs)
+print(Amcmc, mumcmc, sigmcmc, lnfmcmc)
 
 ################################################################################
 ################################# PLOTTING #####################################
