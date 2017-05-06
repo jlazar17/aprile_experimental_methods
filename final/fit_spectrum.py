b1ed4d8ad2732a1b897032db95cc41abe34bcdf0
Baby Cakes <babycakes@pg1.phys.columbia.edu>
2017-05-06 17:33:21 -0400

    added labels to graph

--- final/fit_spectrum.py
+++ final/fit_spectrum.py
@@ -1,9 +1,10 @@
 #! usr/bin/python
 
-import matplotlib.pyplot as plt
 import pickle
 import numpy as np
-import emceeGaussFit2 as gfit
+import emceeGaussFit as gfit
+import matplotlib.pyplot as plt
+from matplotlib import rc
 
 ################################################################################
 ############################## PREPARING ARRAY #################################
@@ -27,13 +28,22 @@ yErrs = np.array([ x**0.5 for x in n ])
 # numbers based on looking at graph
 guess = (140, 33000, 879)
 a, mu, sigma, a_std, mu_std, sigma_std = gfit.getStats(guess, binMids, n, yErrs)
-print(a, mu, sigma)
-print(a_std, mu_std, sigma_std)
+gauss = gfit.gauss((a, mu, sigma), binMids)
 
 ################################################################################
 ################################# PLOTTING #####################################
 
+resultsStr = ('$\mu_{Cs137}=%.2f\pm%.2f ADC Counts$\n$\sigma_{Cs137}='
+			  '%.2f\pm%.2f ADC Counts$'%(mu, mu_std, sigma, sigma_std))
 plt.figure(1)
-plt.subplot(111)
-plt.errorbar(binMids, n, fmt=' ',xerr = binWidth, yerr = yErrs)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+plt.rc('text', usetex=True)
+fig, ax = plt.subplots(1)
+ax.errorbar(binMids, n, fmt=' ',xerr = binWidth, yerr = yErrs)
+ax.plot(binMids, gauss, 'r--')
+ax.set_title('Fitted Integrals')
+ax.set_xlabel('ADC Counts')
+ax.set_ylabel('Counts')
+ax.text(0.05, 0.95, resultsStr, transform=ax.transAxes, fontsize=12,
+        verticalalignment='top')
 plt.savefig('integral_plt.png')
